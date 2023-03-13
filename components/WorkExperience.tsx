a72f2e470a58b0eee8c14751708e1811e5a9baf8
roah019 <roah017@gmail.com>
2023-03-13 15:51:12 +0530

    Final commit before adding to sanity.io

--- components/WorkExperience.tsx
+++ components/WorkExperience.tsx
@@ -13,7 +13,8 @@ const WorkExperience = (props: Props) => {
     className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
 
-        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory'>
+        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory scrollbar scrollbar-track-gray-400/20 
+                     scrollbar-thumb-[#F7AB0A]/80'>
             <ExperienceCard />
             <ExperienceCard />
             <ExperienceCard />
